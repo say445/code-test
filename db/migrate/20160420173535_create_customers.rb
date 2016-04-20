568b6b167e389abf8da2bbaba24f422e24891e2b
Skip Gibson <skipcloudgibson@gmail.com>
2016-04-20 18:36:09 +0100

    Add cutsomer model

--- db/migrate/20160420173535_create_customers.rb
+++ db/migrate/20160420173535_create_customers.rb
@@ -1,6 +1,10 @@
 class CreateCustomers < ActiveRecord::Migration
   def change
     create_table :customers do |t|
+      t.string :organisation
+      t.string :title
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps null: false
     end
